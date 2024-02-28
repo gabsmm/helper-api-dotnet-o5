8ca2fe1fa25f30560103de7c0be81dc8ef731b2c
gabsmm <gaby.medeiros@gmail.com>
2024-02-28 20:58:18 -0300

    Cotacao Moeda Controller

---
+++ helper-api-dotnet-o5/Controllers/ExchangeRateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helper_api_dotnet_o5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRateController : ControllerBase
+    {
+        private readonly ILogger<ExchangeRateController> _logger;
+
+        public ExchangeRateController(ILogger<ExchangeRateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetExchangeRateController")]
+        public string Get()
+        {
+            return "teste";
+        }
+    }
+}
