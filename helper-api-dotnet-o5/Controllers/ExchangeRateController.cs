5e31726a9cb07b06c60815b4ac19ab781ff7f90e
gabsmm <gaby.medeiros@gmail.com>
2024-02-28 23:12:19 -0300

    Cotacao ultimos dias

--- helper-api-dotnet-o5/Controllers/ExchangeRateController.cs
+++ helper-api-dotnet-o5/Controllers/ExchangeRateController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using helper_api_dotnet_o5.Infrastructure;
+using helper_api_dotnet_o5.Models.ExchangeRate;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace helper_api_dotnet_o5.Controllers
 {
@@ -7,6 +9,7 @@ namespace helper_api_dotnet_o5.Controllers
     [ApiController]
     public class ExchangeRateController : ControllerBase
     {
+        private const string ENDPOINT = "https://economia.awesomeapi.com.br/json";
         private readonly ILogger<ExchangeRateController> _logger;
 
         public ExchangeRateController(ILogger<ExchangeRateController> logger)
@@ -14,10 +17,22 @@ namespace helper_api_dotnet_o5.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetExchangeRateController")]
-        public string Get()
+        [HttpGet]
+        [Route("{moeda}/{dias}")]
+        [ProducesResponseType(typeof(List<ExchangeRateOutput>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status502BadGateway)]
+        public IActionResult Get2(string moeda, int dias)
         {
-            return "teste";
+            var route = $"daily/{moeda}/{dias}";
+            var api = new HelperAPI(ENDPOINT);
+            var result = api.MetodoGET<List<ExchangeRateOutput>>(route).Result;
+
+            if (result.Count > 0)
+                return Ok(result);
+            else
+                return NoContent();
         }
     }
 }
