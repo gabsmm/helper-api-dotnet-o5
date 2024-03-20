835501bfec0c6c7c26a7224f3388baeb0065f846
gabsmm <gaby.medeiros@gmail.com>
2024-03-19 23:01:58 -0300

    Inclusão do Projeto de Teste

---
+++ helper-api-test-dotnet-o5/Controllers/ExchangeRateControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using helper_api_dotnet_o5.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace helperapi_test_dotnet.ControllersTest
+{
+    public class ExchangeRateControllerTest
+    {
+        [Fact]
+        public void ExecutaRotaCotacaoGET_QuandoAPIDisponivelEParametroOK_EntaoRetornaObjetoValido()
+        {
+            //Arrange
+            var loggerMock = new Mock<ILogger<ExchangeRateController>>();
+            var controller = new ExchangeRateController(loggerMock.Object);
+
+            //Act
+            var result = controller.Get("USD-BRL", 1);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+
+        [Fact]
+        public void ExecutaRotaCotacaoGET_QuandoMoedaInvalidaEntaoRetornaNotFound()
+        {
+            //Arrange
+            var loggerMock = new Mock<ILogger<ExchangeRateController>>();
+            var controller = new ExchangeRateController(loggerMock.Object);
+
+            //Act
+            var result = controller.Get("USD-BXX", 0);
+
+            //Assert
+            Assert.IsType<StatusCodeResult>(result);
+        }
+    }
+}
---
+++ helper-api-test-dotnet-o5/Infrastructure/HelperApiTest.cs
@@ -0,0 +1,42 @@
+﻿using helper_api_dotnet_o5.Infrastructure;
+using helper_api_dotnet_o5.Models.ExchangeRate;
+
+namespace helperapi_test_dotnet.InfrastructureTest
+
+{
+
+    public class HelperAPITest
+
+    {
+
+        private const string ENDPOINT = "https://economia.awesomeapi.com.br/json";
+
+        [Fact]
+
+        public async Task ExecutaMetodoGET_QuandoAPIERotaValida_EntaoRetornaObjetoValido()
+
+        {
+
+            //Arrange
+
+            var helperAPI = new HelperAPI(ENDPOINT);
+
+            var route = "USD-BRL/1";
+
+
+            //Act
+
+            var result = await helperAPI.MetodoGET<List<ExchangeRateOutput>>(route);
+
+            //Assert
+
+            Assert.NotNull(result);
+
+            Assert.IsType<List<ExchangeRateOutput>>(result);
+
+            Assert.True(result.Count() > 0);
+
+        }
+    }
+
+}
